0db0c2d4774db3d91446a6acfd6dbd04afbd6846
Preetam D'Souza <preetamjdsouza@gmail.com>
2019-07-23 16:42:30 -0400

    imp(example): Make example data consistent
    FMA FTW!

--- example/App.js
+++ example/App.js
@@ -12,17 +12,17 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     const data = [
-        { sender: 'Ali Connors', subject: 'Brunch this weekend?', body: "I'll be in your neighborhood..."},
-        { sender: 'Sandra Adams', subject: 'Oui Oui', body: "Do you have Paris reco..."},
-        { sender: 'Trevor Hansen', subject: 'Birthday Gift', body: "Have any ideas about..."},
-        { sender: 'Edward Elric', subject: 'I figured it out!', body: "Had an insight that we could..."},
-        { sender: 'SpammyCo', subject: 'WEEEKEND SALE!', body: "Don't miss out on our latest sale..."},
-        { sender: 'Winry Rockbell', subject: 'Where the hell is Ed??', body: "Guess what? Ed skipped out..."},
-        { sender: 'SpammyCo2', subject: 'Last chance for 20% OFF!', body: "Don't miss out on our latest sale..."},
-        { sender: 'Winry Rockbell', subject: 'Where the hell is Ed??', body: "Guess what? Ed skipped out..."},
-        { sender: 'Winry Rockbell', subject: 'Where the hell is Ed??', body: "Guess what? Ed skipped out..."},
-        { sender: 'Winry Rockbell', subject: 'Where the hell is Ed??', body: "Guess what? Ed skipped out..."},
-        { sender: 'Winry Rockbell', subject: 'Where the hell is Ed??', body: "Guess what? Ed skipped out..."},
+        { sender: 'Rockbell Automail', subject: 'Your order is ready!', body: "Hi Preetam, the item you ordered is now available for pickup!"},
+        { sender: 'Amestris Kebab', subject: 'Your Receipt', body: "Thank you for dining with us last night!"},
+        { sender: 'Alphonse Elric', subject: 'Recommendations?', body: "Was wondering if you had any recs for Chinese food in the area?"},
+        { sender: 'Edward Elric', subject: 'I figured it out!', body: "Had an insight that we can apply alkahestry to to solve an alchemical paradox!"},
+        { sender: 'SpammyCo', subject: 'WEEEKEND SALE!', body: "Don't miss out on our latest weekend sale!"},
+        { sender: 'Winry Rockbell', subject: 'Where the hell is Ed??', body: "Guess what? Ed skipped out on our family dinner last night."},
+        { sender: 'SpammyCo2', subject: 'Last chance for 20% OFF!', body: "Going once, going twice!"},
+        { sender: 'Roy Mustang', subject: 'Surprise Party', body: "You'd better be at my party next weekend."},
+        { sender: 'Alex Louis Armstrong', subject: 'Gym Meet!', body: "I'm hosting a training event at my gym next month!"},
+        { sender: 'Riza Hawkeye', subject: 'Can you do me a favor?', body: "Have to ask you for a quick favor."},
+        { sender: 'Lin Yao', subject: 'Amestris Trip', body: "I'll be in town this week - wanna grab some food?"},
     ];
     data.forEach((x, i) => x.id = `${i}`); // add unique keys
     this.state = { data };
