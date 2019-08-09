afd11de5ea42847035359e5339c047ba62fe1a5b
Preetam D'Souza <preetamjdsouza@gmail.com>
2019-08-09 15:36:40 -0400

    build: Add jest for testing
    Note that we had to make sure the versions of react, react-dom, and
    react-test-renderer were the same in package.json to avoid errors.

---
+++ components/SwipeActionList.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text } from 'react-native';
+import renderer from 'react-test-renderer';
+
+import SwipeActionList from './SwipeActionList';
+
+describe('<SwipeActionList />', () => {
+  it('renders (smoke test)', () => {
+    const data = [0, 1, 2, 3];
+    const keyExtractor = x => `${x}`;
+    const renderItem = data => <Text>{data.item}</Text>;
+    const renderLeftHiddenItem = () => <Text>left hidden item</Text>
+    const renderRightHiddenItem = () => <Text>right hidden item</Text>
+    const tree = renderer.create(
+      <SwipeActionList
+        data={data}
+        keyExtractor={keyExtractor}
+        renderItem={renderItem}
+        renderLeftHiddenItem={renderLeftHiddenItem}
+        renderRightHiddenItem={renderRightHiddenItem}
+      />
+    ).toJSON();
+    expect(tree.children.length).toBe(1);
+  });
+});
