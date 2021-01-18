b73e0444772e832782a53ecdc9622b4c55ba4735
Preetam D'Souza <preetamjdsouza@gmail.com>
2021-01-18 12:11:35 -0500

    build: Streamline dev flow with example app
    Our previous method of naively using 'file:../' in the example's
    package.json was a poor choice for a few reasons:
    
    1. It would require a redundant copy of things in our example's
       node_modules dir
    2. It would require re-running `yarn install` after every change to
       SwipeActionList, which would take forever since all the files would
    need to be copied over again recursively.
    
    The WAY better solution is to let Metro resolve
    'react-native-swipe-action-list' from the root library directory
    instead. This solves both problems above and lets us use fast refresh
    when developing!

--- example/metro.config.js
+++ example/metro.config.js
@@ -4,9 +4,15 @@
  *
  * @format
  */
+const path = require('path');
 const blacklist = require('metro-config/src/defaults/blacklist');
 
+const reactNativeSwipeActionListRoot = path.resolve(__dirname, '..');
+
 module.exports = {
+  // Since we keep the example app separate from our root project we need
+  // to configure metro to include our lib in the JS bundle it builds.
+  watchFolders: [reactNativeSwipeActionListRoot],
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -16,8 +22,17 @@ module.exports = {
     }),
   },
   resolver: {
+    // Duplicate copies of react-native in the metro bundle stop the app from loading
+    // so blacklist the root project's version from being processed by metro.
     blacklistRE: blacklist([
-      /node_modules\/.*\/node_modules\/react-native\/.*/,
+      new RegExp(
+        `${reactNativeSwipeActionListRoot}/node_modules/react-native/.*`,
+      ),
     ]),
+    // Since we've blacklisted react-native from the root project, let metro resolve
+    // react-native imports from the root project using our example's node_modules instead.
+    extraNodeModules: {
+      'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+    },
   },
 };
